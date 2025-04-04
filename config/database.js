6e220e159f83ebb94ddd0c91e5e65db1ad04033f
EFrega <efrega77@gmail.com>
2025-04-04 00:05:28 -0300

    models update

--- config/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-// Crear la instancia de Sequelize
-const sequelize = new Sequelize('proyectointegrador', 'root', 'Sabbah2505', {
-    host: 'localhost',
-    dialect: 'mysql',
-});
-
-module.exports = sequelize;  // Exportamos la instancia de sequelize
